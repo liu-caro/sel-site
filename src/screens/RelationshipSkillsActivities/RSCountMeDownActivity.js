77ed47b87eca35b500984a0fa8d51de38185bbb4
jaanatabalon <jaanatabalon@gmail.com>
2022-02-02 17:42:38 -0500

    starting work on count me down activity page

--- src/screens/RelationshipSkillsActivities/RSCountMeDownActivity.js
+++ src/screens/RelationshipSkillsActivities/RSCountMeDownActivity.js
@@ -1,9 +1,13 @@
-const ExamplePage = () => {
+import { Container, Typography } from "@mui/material";
+
+const RSCountMeDownActivity = () => {
   return (
-      <>
-      This is an example page. 
-      </>
+      <Container maxWidth="sm">
+        <Typography variant="h4">Count Me Down</Typography>
+
+        <Typography variant="body1">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</Typography>
+      </Container>
   );
 };
 
-export default ExamplePage;
+export default RSCountMeDownActivity;
