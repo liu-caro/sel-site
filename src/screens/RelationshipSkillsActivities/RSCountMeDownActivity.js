171ab9425525a8046e4ab20d98cc24da55e7cf0b
jaanatabalon <jaanatabalon@gmail.com>
2022-02-09 15:35:34 -0500

    addressed changes: replaced filler text, added grids around components, removed disabled button, consistent button variants

--- src/screens/RelationshipSkillsActivities/RSCountMeDownActivity.js
+++ src/screens/RelationshipSkillsActivities/RSCountMeDownActivity.js
@@ -1,23 +1,61 @@
-import { Container, Typography } from "@mui/material";
+import { Grid, Container, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import ReactPlayer from 'react-player'
 
 const RSCountMeDownActivity = () => {
   return (
-      <Container maxWidth="sm">
-        <Typography variant="h2">Count Me Down</Typography>
-
-        <Typography variant="body1">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</Typography>
-        <Button variant="contained" disabled>Printable Version</Button>
-      
-        <Typography variant="h3">Directions</Typography>
-        <Typography variant="body1">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</Typography>
+    <Grid
+      container
+      direction="column"
+      justifyContent="center"
+      spacing={2}
+      p={8}
+    >
+        <Grid item xs={12}>
+          <Typography variant="h2">Count Me Down</Typography>
+        </Grid>
         
-        <Typography variant="h3">Instructional Video</Typography>
-        <ReactPlayer url="https://youtu.be/4S2yj4TB_I8" />
+        <Grid item xs={12}>
+          <Typography variant="body1">Count Me Down is a strategy from Move This World that helps students identify ways they are similar and different from others. The goals are to develop a stronger understanding of peers and to demonstrate respect for differences and exhibit empathy. An example prompt for Count Me Down is as follows:
+          <ul>
+            <li>3 unique things about your family</li>
+            <li>2 challenges you're currently facing</li>
+            <li>1 goal or dream you have</li>
+          </ul>
+          </Typography>
+        </Grid>
 
-        <Button variant="outlined">Mark as done!</Button>
-      </Container>
+        <Grid item xs={12}>
+          <Button variant="contained">Printable Version</Button>
+        </Grid>
+      
+        <Grid item xs={12}>
+          <Typography variant="h3">Directions</Typography>
+        </Grid>
+        
+        <Grid item xs={12}>
+          <Typography variant="body1">Ask your child the following questions, one by one:
+          <ul>
+            <li>3 unique things about your family</li>
+            <li>2 challenges you're currently facing</li>
+            <li>1 goal or dream you have</li>
+          </ul>
+          Listen to their answers and discuss with them their thoughts. Ask them to explain their thought process and why they chose their answer(s). Be sure to share your own answers to each of the questions as well.
+          </Typography>
+        </Grid>
+        
+        <Grid item xs={12}>
+          <Typography variant="h3">Instructional Video</Typography>
+        </Grid>
+
+        <Grid item xs={12}>
+          <ReactPlayer url="https://youtu.be/4S2yj4TB_I8" />
+        </Grid>
+
+        <Grid item xs={12}>
+          <Button variant="contained">Mark as done!</Button>
+        </Grid>
+    </Grid>
   );
 };
 
