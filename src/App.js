c14e872978178fd3f23c6f939eaeeaf46fe77e7f
jaanatabalon <jaanatabalon@gmail.com>
2022-02-14 21:21:49 -0500

    added Onboarding pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import ActivitiesPage from "./screens/ActivitiesPage";
 import ExamplePage from "./screens/ExamplePage";
 import RelationshipSkillsPage from './screens/SelCategories/RelationshipSkillsPage'
 import RSCountMeDownActivity from "./screens/RelationshipSkillsActivities/RSCountMeDownActivity";
+import OnboardingWelcomePage from "./screens/OnboardingPages/OnboardingWelcomePage";
+import OnboardingActivitiesPage from "./screens/OnboardingPages/OnboardingActivitiesPage";
+import OnboardingPrintablesPage from "./screens/OnboardingPages/OnboardingPrintablesPage";
 import LandingPage from "./screens/LandingPage";
 import { theme } from "./utils/theme";
 
@@ -20,6 +23,9 @@ const App = () => {
           <Route path="/categories/relationship-skills" element={<RelationshipSkillsPage />} />
           <Route path="/example" element={<ExamplePage />} />
           <Route path="/rs/count-me-down" element={<RSCountMeDownActivity />} />
+          <Route path="/onboarding-welcome" element={<OnboardingWelcomePage />} />
+          <Route path="/onboarding-activities" element={<OnboardingActivitiesPage />} />
+          <Route path="/onboarding-printables" element={<OnboardingPrintablesPage />} />
           <Route path="*" element={<HomePage />} />
         </Routes>
       </Router>
