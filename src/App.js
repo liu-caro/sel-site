a6c65d94711c4cba91a61aedecd61e0a45dccb1f
Caroline Liu <caroline.liu111@gmail.com>
2022-02-16 15:31:07 -0500

    Cari update menu (#4)
    * set up responsive menu bar
    
    * hook up menu bar to all pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import { ThemeProvider } from "@mui/system";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Drawer from "./components/Drawer";
-import HomePage from "./screens/HomePage";
-import ActivitiesPage from "./screens/ActivitiesPage";
-import ExamplePage from "./screens/ExamplePage";
-import RelationshipSkillsPage from './screens/SelCategories/RelationshipSkillsPage'
-import RSCountMeDownActivity from "./screens/RelationshipSkillsActivities/RSCountMeDownActivity";
-import LandingPage from "./screens/LandingPage";
-import { theme } from "./utils/theme";
+import { ThemeProvider } from '@mui/system';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MenuBar from './components/MenuBar';
+import HomePage from './screens/HomePage';
+import ActivitiesPage from './screens/ActivitiesPage';
+import ProgressPage from './screens/ProgressPage';
+import ResourcesPage from './screens/ResourcesPage';
+import PrintablesPage from './screens/PrintablesPage';
+import RelationshipSkillsPage from './screens/SelCategories/RelationshipSkillsPage';
+import RSCountMeDownActivity from './screens/RelationshipSkillsActivities/RSCountMeDownActivity';
+import ExamplePage from './screens/ExamplePage';
+import LandingPage from './screens/LandingPage';
+import { theme } from './utils/theme';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <Drawer />
+        <MenuBar />
         <Routes>
+          {/* home page */}
           <Route path="/home" element={<HomePage />} />
+
+          {/* main pages */}
           <Route path="/activities" element={<ActivitiesPage />} />
-          <Route path="/categories/relationship-skills" element={<RelationshipSkillsPage />} />
-          <Route path="/example" element={<ExamplePage />} />
+          <Route path="/progress" element={<ProgressPage />} />
+          <Route path="/resources" element={<ResourcesPage />} />
+          <Route path="/printables" element={<PrintablesPage />} />
+
+          {/* SEL category pages */}
+          <Route
+            path="/categories/relationship-skills"
+            element={<RelationshipSkillsPage />}
+          />
+
+          {/* SEL activities pages */}
+          {/* activites: relationship skills */}
           <Route path="/rs/count-me-down" element={<RSCountMeDownActivity />} />
+
+          <Route path="/example" element={<ExamplePage />} />
           <Route path="*" element={<HomePage />} />
         </Routes>
       </Router>
---
+++ src/screens/ProgressPage.js
@@ -0,0 +1,9 @@
+const ProgressPage = () => {
+  return (
+      <>
+      This is the Progress page. 
+      </>
+  );
+};
+
+export default ProgressPage;
