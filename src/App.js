4ca9b622dd1a94e26a0e391b7ce3bfa8b3e619aa
Caroline Liu <caroline.liu111@gmail.com>
2022-02-16 15:07:10 -0500

    set up responsive menu bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import { ThemeProvider } from "@mui/system";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Drawer from "./components/Drawer";
-import HomePage from "./screens/HomePage";
-import ActivitiesPage from "./screens/ActivitiesPage";
-import ExamplePage from "./screens/ExamplePage";
-import RelationshipSkillsPage from './screens/SelCategories/RelationshipSkillsPage'
-import RSCountMeDownActivity from "./screens/RelationshipSkillsActivities/RSCountMeDownActivity";
-import LandingPage from "./screens/LandingPage";
-import { theme } from "./utils/theme";
+import { ThemeProvider } from '@mui/system';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MenuBar from './components/MenuBar';
+import HomePage from './screens/HomePage';
+import ActivitiesPage from './screens/ActivitiesPage';
+import ExamplePage from './screens/ExamplePage';
+import RelationshipSkillsPage from './screens/SelCategories/RelationshipSkillsPage';
+import RSCountMeDownActivity from './screens/RelationshipSkillsActivities/RSCountMeDownActivity';
+import LandingPage from './screens/LandingPage';
+import { theme } from './utils/theme';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Router>
-        <Drawer />
+        <MenuBar />
         <Routes>
           <Route path="/home" element={<HomePage />} />
           <Route path="/activities" element={<ActivitiesPage />} />
-          <Route path="/categories/relationship-skills" element={<RelationshipSkillsPage />} />
+          <Route
+            path="/categories/relationship-skills"
+            element={<RelationshipSkillsPage />}
+          />
           <Route path="/example" element={<ExamplePage />} />
           <Route path="/rs/count-me-down" element={<RSCountMeDownActivity />} />
           <Route path="*" element={<HomePage />} />
