4007b8b2405564fb0604a677b274d24ab9995e25
jaanatabalon <jaanatabalon@gmail.com>
2022-02-17 19:31:57 -0500

    completed onboarding pages

--- src/screens/OnboardingPages/OnboardingPrintablesPage.js
+++ src/screens/OnboardingPages/OnboardingPrintablesPage.js
@@ -1,5 +1,7 @@
 import { Grid, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
+import progressTracker from './../../assets/onboardingImages/progressTracker.svg';
+import flashcards from './../../assets/onboardingImages/flashcards.svg';
 
 const OnboardingPrintablesPage = () => {
   return (
@@ -11,23 +13,35 @@ const OnboardingPrintablesPage = () => {
       spacing={2}
       p={8}
     >
-        <Grid item xs={12}>
-          <Typography variant="h1">Enhance with printables</Typography>
-        </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h1">Enhance with printables</Typography>
+      </Grid>
 
-        <Grid item xs={12}>
-            {/* images and bars */}
-        </Grid>
+      <Grid item xs={12}>
+        <img src={progressTracker} alt="progress tracker image" height={650} />
+      </Grid>
 
-        <Grid item xs={12}>
-          <Typography variant="body1">
-            We provide lots of low-cost printable resources that go along with our SEL activities. The home Progress Tracker lets children visually see their progress. The Activity Booklet lets families do activities while travelling. We hope you and your family enjoy growing your SEL skills together!
-          </Typography>
-        </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h4">Progress Tracker</Typography>
+      </Grid>
 
-        <Grid item xs={12}>
-          <Button variant="contained">Get started!</Button>
-        </Grid>
+      <Grid item xs={12}>
+        <img src={flashcards} alt="flashcards image" height={650} />
+      </Grid>
+
+      <Grid item xs={12}>
+        <Typography variant="h4">On-the-Go Activity Flipbook</Typography>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Typography variant="body1">
+          We provide lots of low-cost printable resources that go along with our SEL activities. The home Progress Tracker lets children visually see their progress. The Activity Booklet lets families do activities while travelling. We hope you and your family enjoy growing your SEL skills together!
+        </Typography>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Button variant="contained">Get started!</Button>
+      </Grid>
 
     </Grid>
   );
