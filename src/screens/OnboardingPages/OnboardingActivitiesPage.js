b2267f5c8d099bf0b94ed29e627b3ac044bdb2b3
jaanatabalon <jaanatabalon@gmail.com>
2022-02-17 19:31:57 -0500

    completed onboarding pages

--- src/screens/OnboardingPages/OnboardingActivitiesPage.js
+++ src/screens/OnboardingPages/OnboardingActivitiesPage.js
@@ -1,5 +1,6 @@
 import { Grid, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
+import activitiesPage from './../../assets/onboardingImages/activitiesPage.svg';
 
 const OnboardingActivitiesPage = () => {
   return (
@@ -11,27 +12,27 @@ const OnboardingActivitiesPage = () => {
       spacing={2}
       p={8}
     >
-        <Grid item xs={12}>
-          <Typography variant="h1">Practice skills with activities</Typography>
-        </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h1">Practice skills with activities</Typography>
+      </Grid>
 
-        <Grid item xs={12}>
-            {/* images and bars */}
-        </Grid>
+      <Grid item xs={12}>
+        <img src={activitiesPage} alt="activities page" height={650} />
+      </Grid>
 
-        <Grid item xs={12}>
-          <Typography variant="body1">
-            Each SEL pillar has its own page with related activities for your child to practice that specific skill. You can learn about how the activity builds the skill, print out the instructions, watch an instructional video, and track your child&apos;s completion!          
-          </Typography>
-        </Grid>
+      <Grid item xs={12}>
+        <Typography variant="body1">
+          Each SEL pillar has its own page with related activities for your child to practice that specific skill. You can learn about how the activity builds the skill, print out the instructions, watch an instructional video, and track your child&apos;s completion!
+        </Typography>
+      </Grid>
 
-        <Grid item xs={12}>
-          <Button variant="text">Skip tutorial</Button>
-        </Grid>
+      <Grid item xs={12}>
+        <Button variant="text">Skip tutorial</Button>
+      </Grid>
 
-        <Grid item xs={12}>
-          <Button variant="contained">Next</Button>
-        </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained">Next</Button>
+      </Grid>
 
     </Grid>
   );
