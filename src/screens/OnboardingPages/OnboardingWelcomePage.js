10c1fd4a0d2be9144dbacd2aa6d9103db27e251c
jaanatabalon <jaanatabalon@gmail.com>
2022-02-17 19:33:06 -0500

    removed commented ImageList

--- src/screens/OnboardingPages/OnboardingWelcomePage.js
+++ src/screens/OnboardingPages/OnboardingWelcomePage.js
@@ -42,55 +42,6 @@ const OnboardingWelcomePage = () => {
         </ImageListItem>
       </ImageList>
 
-      {/* <ImageList sx={{ width: 600 }} cols={2} gap={80}>
-        
-          <ImageListItem>
-            <img src={turtleCharacter} alt="turtle"/>
-           </ImageListItem> 
-
-           <ImageListItem>
-            <Typography variant = "h5">Responsible Decision Making</Typography>
-            <LinearProgress variant="determinate" value={80} sx={{height: 30}} />
-           </ImageListItem>
-           
-           <ImageListItem>
-            <img src={dolphinCharacter} alt="dolphin"/>
-           </ImageListItem>
-
-           <ImageListItem>
-            <Typography variant = "h5">Self Awareness</Typography>
-            <LinearProgress variant="determinate" value={60} sx={{height: 30}}/>
-           </ImageListItem>   
-
-           <ImageListItem>
-            <img src={giraffeCharacter} alt="giraffe"/>
-           </ImageListItem>
-           
-           <ImageListItem>
-            <Typography variant = "h5">Social Awareness</Typography>
-            <LinearProgress variant="determinate" value={25} sx={{height: 30}}/>
-           </ImageListItem>
-
-           <ImageListItem>
-            <img src={tigerCharacter} alt="tiger"/>
-           </ImageListItem>
-
-           <ImageListItem>
-            <Typography variant = "h5">Relationship Skills</Typography>
-            <LinearProgress variant="determinate" value={85} sx={{height: 30}}/>
-           </ImageListItem> 
-
-           <ImageListItem>
-            <img src={hippoCharacter} alt="hippo"/>
-           </ImageListItem>
-           
-           <ImageListItem>
-            <Typography variant = "h5">Self Management</Typography>
-            <LinearProgress variant="determinate" value={40} sx={{height: 30}}/>
-           </ImageListItem>
-        </ImageList> */}
-
-
       <Grid item xs={12}>
         <Typography variant="body1">
           SEL has 5 main pillars of learning, represented by 5 animals. We provide activities for parents to do with their children to develop SEL skills As your child improves on their skills, they will level up in the related category.
