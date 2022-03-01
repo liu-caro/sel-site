82f773a00818756aa61e0d2ebe6612d168f1b11a
Caroline Liu <caroline.liu111@gmail.com>
2022-03-01 02:08:44 -0500

    signup complete

--- src/screens/SignupPage.js
+++ src/screens/SignupPage.js
@@ -1,64 +1,148 @@
-import { Grid, Typography, Button } from '@mui/material';
+import { Alert, Grid, Typography } from '@mui/material';
+import LoadingButton from '@mui/lab/LoadingButton';
 import TextField from '@mui/material/TextField';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { firebaseAuth } from '../firebase-config';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import useInput from '../hooks/useInput';
+import { useState } from 'react';
 
 const SignupPage = () => {
+    const email = useInput('');
+    const password = useInput('');
+    const [error, setError] = useState();
+    let navigate = useNavigate();
+    const [isLoading, setIsLoading] = useState(false);
     return (
         <Grid
             container
-            direction="column"
-            justifyContent="center"
-            alignItems="center"
+            direction='column'
+            justifyContent='center'
+            alignItems='center'
             spacing={2}
             p={8}
         >
             <Grid item xs={12} mb={2}>
-                <Typography variant="h1" align='center'>Create an Account</Typography>
+                <Typography variant='h1' align='center'>
+                    Create an Account
+                </Typography>
             </Grid>
+
+            {error ? (
+                <Alert severity='error'>
+                    The following error occured when creating your account:
+                    <br />
+                    {error.errorMessage}
+                </Alert>
+            ) : null}
 
             <Grid item xs={12} style={{ maxWidth: 500 }} mb={4}>
-                <Typography variant="body2" align='left'>Users can browse all activities without an account,
-                    but with an account you can keep track of your child’s progress in SEL skills and we can recommend
-                    activities which are best suited for your child’s age!</Typography>
+                <Typography variant='body2' align='left'>
+                    Users can browse all activities without an account, but with
+                    an account you can keep track of your child’s progress in
+                    SEL skills and we can recommend activities which are best
+                    suited for your child’s age!
+                </Typography>
                 <br />
-                <Typography variant="body2" align='left'>We currently do not and will never share your information
-                    with third parties.</Typography>
+                <Typography variant='body2' align='left'>
+                    We currently do not and will never share your information
+                    with third parties.
+                </Typography>
             </Grid>
 
-            <Grid container spacing={2} style={{ maxWidth: 500 }} justifyContent="center">
+            <Grid
+                container
+                spacing={2}
+                style={{ maxWidth: 500 }}
+                justifyContent='center'
+            >
                 <Grid item xs={12}>
-                    <TextField fullWidth id="filled-basic" label="Child's Age" variant="filled" />
+                    <TextField
+                        fullWidth
+                        id='child-age'
+                        label="Child's Age"
+                        variant='filled'
+                    />
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField fullWidth id="filled-basic" label="Email" variant="filled" />
+                    <TextField
+                        fullWidth
+                        id='email'
+                        label='Email'
+                        variant='filled'
+                        value={email.value}
+                        onChange={email.onChange}
+                    />
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField fullWidth id="filled-basic" label="Confirm Email" variant="filled" />
+                    <TextField
+                        fullWidth
+                        id='confirm-email'
+                        label='Confirm Email'
+                        variant='filled'
+                    />
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField fullWidth id="filled-basic" label="Create Password" variant="filled" />
+                    <TextField
+                        fullWidth
+                        id='password'
+                        label='Create Password'
+                        type='password'
+                        variant='filled'
+                        value={password.value}
+                        onChange={password.onChange}
+                    />
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField fullWidth id="filled-basic" label="Confirm Password" variant="filled" />
+                    <TextField
+                        fullWidth
+                        id='confirm-password'
+                        label='Confirm Password'
+                        type='password'
+                        variant='filled'
+                    />
                 </Grid>
 
-                <Grid item align={"center"} xs={12}>
-                        <Typography variant="subtitle1">
-                            <span> Returning User? </span>
-                            <Link to="/">
-                                Log in here
-                            </Link>
-                        </Typography>
+                <Grid item align={'center'} xs={12}>
+                    <Typography variant='subtitle1'>
+                        <span> Returning User? </span>
+                        <Link to='/'>Log in here</Link>
+                    </Typography>
                 </Grid>
-                <Grid item align={"center"} xs={12}>
-                    <Link to='/onboarding-welcome' style={{ textDecoration: 'none' }}>
-                        <Button variant="contained">Sign Up!</Button>
-                    </Link>
+                <Grid item align={'center'} xs={12}>
+                    <LoadingButton
+                        loading={isLoading}
+                        variant='contained'
+                        onClick={() => {
+                            setIsLoading(true);
+                            createUserWithEmailAndPassword(
+                                firebaseAuth,
+                                email.value,
+                                password.value
+                            )
+                                .then((userCredential) => {
+                                    // Signed in
+                                    const user = userCredential.user;
+                                    console.log(user);
+                                    navigate('/onboarding-welcome');
+                                })
+                                .catch((error) => {
+                                    const errorCode = error.code;
+                                    const errorMessage = error.message;
+                                    console.log(
+                                        'Error occured with ' +
+                                            { errorCode } +
+                                            { errorMessage }
+                                    );
+                                    setError({ errorCode, errorMessage });
+                                })
+                                .finally(() => setIsLoading(false));
+                        }}
+                    >
+                        Sign Up!
+                    </LoadingButton>
                 </Grid>
             </Grid>
-
-
-
         </Grid>
     );
 };
