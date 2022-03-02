7276f26fae706359d1faf27a6f72e8e8accceb0d
Caroline Liu <caroline.liu111@gmail.com>
2022-03-02 13:59:54 -0500

    signup complete (#13)

---
+++ src/firebase-config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: 'sel-zoo.firebaseapp.com',
+    databaseURL: 'https://sel-zoo-default-rtdb.firebaseio.com',
+    projectId: 'sel-zoo',
+    storageBucket: 'sel-zoo.appspot.com',
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const firebaseAuth = getAuth(app);
+
+export { firebaseAuth };
--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -3,44 +3,53 @@ import PlaceIcon from '@mui/icons-material/Place';
 import Card from '../components/Card';
 
 const cardInfo = [
-  [
-    ['Activities', <PlaceIcon />, '/activities'],
-    ['Progress', <PlaceIcon />],
-  ],
-  [
-    ['Resources', <PlaceIcon />],
-    ['Printables', <PlaceIcon />],
-  ],
+    [
+        ['Activities', <PlaceIcon />, '/activities'],
+        ['Progress', <PlaceIcon />],
+    ],
+    [
+        ['Resources', <PlaceIcon />],
+        ['Printables', <PlaceIcon />],
+    ],
 ];
 
 const HomePage = () => {
-  return (
-    <Grid
-      container
-      direction="column"
-      justifyContent="center"
-      alignItems="center"
-      spacing={2}
-    >
-      <Grid item xs={12}>
-        <Typography variant="h4">SEL Portal Homepage </Typography>
-      </Grid>
-      <br />
-      {cardInfo.map((row) => {
-        return (
-          <Grid container xs={6} spacing={2} style={{ marginBottom: '0.5rem' }}>
-            {row.map(([name, image, internalLink]) => {
-              return (
-                <Grid item key={name} xs={6}>
-                  <Card title={name} icon={image} internalLink={internalLink} />
-                </Grid>
-              );
+    return (
+        <Grid
+            container
+            direction='column'
+            justifyContent='center'
+            alignItems='center'
+            spacing={2}
+        >
+            <Grid item xs={12}>
+                <Typography variant='h4'>SEL Portal Homepage </Typography>
+            </Grid>
+            <br />
+            {cardInfo.map((row) => {
+                return (
+                    <Grid
+                        container
+                        xs={6}
+                        spacing={2}
+                        style={{ marginBottom: '0.5rem' }}
+                    >
+                        {row.map(([name, image, internalLink]) => {
+                            return (
+                                <Grid item key={name} xs={6}>
+                                    <Card
+                                        title={name}
+                                        icon={image}
+                                        internalLink={internalLink}
+                                    />
+                                </Grid>
+                            );
+                        })}
+                    </Grid>
+                );
             })}
-          </Grid>
-        );
-      })}
-    </Grid>
-  );
+        </Grid>
+    );
 };
 
 export default HomePage;
