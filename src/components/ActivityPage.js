3d1c12503b7f26ab29175b316d408a4987a5de85
delacruzralph <55125034+delacruzralph@users.noreply.github.com>
2022-04-05 15:11:34 -0400

    activity mark as done button confetti fall (#24)
    * created sel category pages
    
    * added sel categories text content
    
    * relationship skills activities added (still need to add rating/time)
    
    * responsible decision making activities
    
    * self awareness activities
    
    * social awareness activities
    
    * self management activities
    
    * added links for activities
    
    * added links for SEL categories
    
    * typo fixes
    
    * fixed activities links on category pages, changed buttonCategory classes, updated links (removed /categories/...)
    
    * fixed merge conflicts
    
    * mark as done button makes confetti fall

--- src/components/ActivityPage.js
+++ src/components/ActivityPage.js
@@ -1,8 +1,31 @@
 import { Grid, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
-import ReactPlayer from 'react-player'
+import ReactPlayer from 'react-player';
+import ConfettiGenerator from 'confetti-js';
+import DolphinSVG from '../assets/sel-animals/DolphinSVG.svg';
+import HippoSVG from '../assets/sel-animals/HippoSVG.svg';
+import GiraffeSVG from '../assets/sel-animals/GiraffeSVG.svg';
+import TurtleSVG from '../assets/sel-animals/TurtleSVG.svg';
+import TigerSVG from '../assets/sel-animals/TigerSVG.svg';
 
 const ActivityPage = ({ title, overviewText, directionsText, videoUrl, buttonCategory }) => {
+
+  const handleClick = (() => {
+    const confettiSettings = {
+      target: 'my-canvas',
+      props: [{ type: 'svg', src: DolphinSVG },
+      { type: 'svg', src: HippoSVG },
+      { type: 'svg', src: GiraffeSVG },
+      { type: 'svg', src: TurtleSVG },
+      { type: 'svg', src: TigerSVG }],
+      respawn: false,
+      clock: 20
+    };
+
+    const confetti = new ConfettiGenerator(confettiSettings);
+    confetti.render();
+  });
+
   return (
     <Grid
       container
@@ -12,37 +35,38 @@ const ActivityPage = ({ title, overviewText, directionsText, videoUrl, buttonCat
       spacing={2}
       p={4}
     >
-        <Grid item xs={12}>
-          <Typography variant="h1">{title}</Typography>
-        </Grid>
-        
-        <Grid item xs={12}>
-          <Typography variant="body1"> {overviewText} </Typography>
-        </Grid>
+      <canvas id='my-canvas' style={{ position: 'absolute' }} />
+      <Grid item xs={12}>
+        <Typography variant="h1">{title}</Typography>
+      </Grid>
 
-        <Grid item xs={12}>
-          <Button variant="contained" class={buttonCategory}>Printable Version</Button>
-        </Grid>
-      
-        <Grid item xs={12}>
-          <Typography variant="h2">Directions</Typography>
-        </Grid>
-        
-        <Grid item xs={12}>
-          <Typography variant="body1"> {directionsText} </Typography>
-        </Grid>
-        
-        <Grid item xs={12}>
-          <Typography variant="h2">Instructional Video</Typography>
-        </Grid>
+      <Grid item xs={12}>
+        <Typography variant="body1"> {overviewText} </Typography>
+      </Grid>
 
-        <Grid item xs={6}>
-          <ReactPlayer url={videoUrl} style={{ maxWidth: '300px'}}/>
-        </Grid>
+      <Grid item xs={12}>
+        <Button variant="contained" class={buttonCategory}>Printable Version</Button>
+      </Grid>
 
-        <Grid item xs={12}>
-          <Button variant="contained" class={buttonCategory}>Mark as done!</Button>
-        </Grid>
+      <Grid item xs={12}>
+        <Typography variant="h2">Directions</Typography>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Typography variant="body1"> {directionsText} </Typography>
+      </Grid>
+
+      <Grid item xs={12}>
+        <Typography variant="h2">Instructional Video</Typography>
+      </Grid>
+
+      <Grid item xs={6}>
+        <ReactPlayer url={videoUrl} style={{ maxWidth: '300px' }} />
+      </Grid>
+
+      <Grid item xs={12}>
+        <Button onClick={handleClick} variant="contained" class={buttonCategory}>Mark as done!</Button>
+      </Grid>
     </Grid>
   );
 };
